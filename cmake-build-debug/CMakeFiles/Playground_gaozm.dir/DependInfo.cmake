23e160e83efaa840542d35539661fcbcd9fb2c5f
Gao Zheming <gao_zm87@yahoo.com>
2021-04-20 12:52:23 +0200

    dailt update

---
+++ cmake-build-debug/CMakeFiles/Playground_gaozm.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Gao_z/CLionProjects/Playground_gaozm/CppProject/main.cpp" "C:/Users/Gao_z/CLionProjects/Playground_gaozm/cmake-build-debug/CMakeFiles/Playground_gaozm.dir/CppProject/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Playground_gaozm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Playground_gaozm.dir/CppProject/main.cpp.obj"
+  "Playground_gaozm.exe"
+  "Playground_gaozm.exe.manifest"
+  "Playground_gaozm.pdb"
+  "libPlayground_gaozm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Playground_gaozm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
